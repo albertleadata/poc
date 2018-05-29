7aa4e9ed84626cb9854d5b976526e8e666274b59
Matt Samudio <matt@samudio.net>
2018-05-28 20:00:15 -0500

    Added basic page

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+function genPage() {
+	printf( "<html>\n");
+	printf( "<body>\n");
+	printf( "<table align=center>\n");
+	printf( "<tr><td align=center><h1>Hello world !</h1></td></tr>\n");
+	printf( "<tr><td align=center><i>... just a test</i></td></tr>\n");
+	printf( "</table>\n");
+	printf( "</body>\n");
+	printf( "</html>\n");
+	return;
+}
+
+genPage();
+?>
